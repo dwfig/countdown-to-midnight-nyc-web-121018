e1d13f0c94f0b6901a5726cf2ac3d59e96b32586
Daniel Fries <daniel.w.fries@gmail.com>
2018-11-25 20:18:22 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(seconds)
+  phrase = "HAPPY NEW YEAR!"
+  while seconds > 0
+    seconds -=1 
+    puts "#{seconds} SECOND(S)!"
+  end
+  phrase
 end
